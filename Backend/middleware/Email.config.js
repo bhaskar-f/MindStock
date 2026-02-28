644140e4553a8e22ec18a7350d510099f4a003da
Bhaskar Sarkar <bat.web.off@gmail.com>
2026-02-28 23:23:41 +0530

    Update email configuration to use environment variables

--- Backend/middleware/Email.config.js
+++ Backend/middleware/Email.config.js
@@ -5,7 +5,7 @@ export const transporter = nodemailer.createTransport({
   port: 587,
   secure: false, // Use true for port 465, false for port 587
   auth: {
-    user: "maxrouteemail@gmail.com",
-    pass: "twuj mniz akqf gcyl",
+    user: process.env.MAIN_EMAIL,
+    pass: process.env.MAIN_PASSWORD,
   },
 });
